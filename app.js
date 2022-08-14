c4570738a57ca29a59ef83ac62b566795bf17863
Arman Augusto <arman.augusto@gmail.com>
2022-08-13 23:28:15 -0700

    added center dot to clock

--- app.js
+++ app.js
@@ -9,11 +9,6 @@ function getTime() {
     const hours = now.getHours()
     const timeInterval = 6
 
-    console.log(now)
-    console.log(seconds * timeInterval)
-    console.log(minutes * timeInterval + seconds / 10)
-    console.log(hours * 30 + minutes / 2)
-
     secondsHand.style.transform = 'rotate(' + (seconds * timeInterval) + 'deg)'
     minutesHand.style.transform = 'rotate(' + (minutes * timeInterval + seconds / 10) + 'deg)'
     hoursHand.style.transform = 'rotate(' + (hours * 30 + minutes / 2) + 'deg)'
